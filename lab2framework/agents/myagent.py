77f6ec02454f24d962c849a28de2a88fd40487d7
Plutzz <benjiongco@gmail.com>
2024-11-17 11:47:36 -0800

    Remember hints between rounds

--- lab2framework/agents/myagent.py
+++ lab2framework/agents/myagent.py
@@ -16,44 +16,69 @@ def format_hint(h):
 class MyAgent(agent.Agent):
     def __init__(self, name, pnr):
         self.name = name
+        self.colorHintsToProcess = {}
+        self.rankHintsToProcess = {}
         self.hints = {}
         self.pnr = pnr
         self.explanation = []
         self.hand = None
-        self.color_hint = None
-        self.rank_hint = None
     def get_action(self, nr, hands, knowledge, trash, played, board, valid_actions, hints, hits, cards_left):
         
+        for player,hand in enumerate(hands):
+            for card_index in range(5):
+                if (player,card_index) not in self.hints:
+                    self.hints[(player,card_index)] = set()
+            if (player) not in self.colorHintsToProcess:
+                self.colorHintsToProcess[(player)] = ""
+            if (player) not in self.rankHintsToProcess:
+                self.rankHintsToProcess[(player)] = ""
+
         my_knowledge = knowledge[nr]
+
+
+        # Process color hints
+        for h, c in self.colorHintsToProcess.items():
+            for i in range(5):
+                if(util.has_property(util.has_color(c), my_knowledge[i])):
+                    print("Card " + str(i) + " is color " + str(c))
+                    self.hints[(h,i)].add(format_hint(HINT_COLOR))
+                    # known[i] += format_hint(HINT_COLOR)
+            self.colorHintsToProcess[h] = ""
+
+        # Process rank hints
+        for h, r in self.rankHintsToProcess.items():
+            for i in range(5):
+                if(util.has_property(util.has_rank(r), my_knowledge[i])):
+                    print("Card " + str(i) + "is rank " + str(r))
+                    self.hints[(h,i)].add(format_hint(HINT_RANK))
+                    # known[i] += format_hint(HINT_RANK)
+            self.rankHintsToProcess[h] = ""
+        
+        # Known hints about myself
         known = [""]*5
 
+        for card_index in range(5):
+            for hint in self.hints[(self.pnr, card_index)]:
+                print("card number" + str(card_index) + " " + str(hint))
+                known[card_index] += str(hint)
 
-        if self.color_hint != None:
-            print("GOT COLOR HINT")
-            for i in range(5):
-                if(util.has_property(util.has_color(self.color_hint), my_knowledge[i])):
-                    print("Card " + str(i) + " is color " + str(self.color_hint))
-                    known[i] += format_hint(HINT_COLOR)
-            self.color_hint = None
-
-        if self.rank_hint != None:
-            print("GOT RANK HINT")
-            for i in range(5):
-                if(util.has_property(util.has_rank(self.rank_hint), my_knowledge[i])):
-                    print("Card " + str(i) + "is rank " + str(self.rank_hint))
-                    known[i] += format_hint(HINT_RANK)
-            self.rank_hint = None
-        
-        
         # Get current chop card (-1 means played does not have a chop card)
         for i in range(4, -1, -1):
             if known[i] == "":
                 print("chop card found")
                 known[i] += "chop"
+                chop = i
+                break
+
+        # Get next player's chop
+        nextPlayerChop = -1
+        for i in range(4, -1, -1):
+            if len(self.hints[(self.pnr + 1 % len(hands)), i]) == 0:
+                nextPlayerChop = i
                 break
 
 
-        self.explanation = [["hints known:"] + known]
+        self.explanation = [["next player's chop card = " + str(nextPlayerChop + 1) + "| hints known: "] + known]
         # Check for playable cards while keeping track of useless cards in the case that we don't find any playable ones
         for i,k in enumerate(my_knowledge):
             if util.is_playable(k, board):
@@ -69,10 +94,9 @@ class MyAgent(agent.Agent):
                                 return Action(HINT_COLOR, player=player, color=card.color)
                             return Action(HINT_RANK, player=player, rank=card.rank)
 
-        # # If we could not play a card and have found a useless card, discard the rightmost useless card
-        # if chop:
-        #     # print(str(max(potential_discards)) + str(potential_discards))
-        #     return Action(DISCARD, card_index=max(potential_discards))
+        # If we could not play a card and have found a useless card, discard the rightmost useless card
+        if chop:
+            return Action(DISCARD, card_index=chop)
         
 
         # HINTS:
@@ -94,9 +118,6 @@ class MyAgent(agent.Agent):
             debug+=" Hint Rank "
 
         print(debug + " | card ind: " + str(action.card_index) + " | color: " + str(action.color) + " | rank: " + str(action.rank))
-        # We only care about the hints that WE got
-        if action.player != self.pnr:
-            return
         # If we play or discard, move our explanation accordingly
         if action.type in [PLAY, DISCARD]:
             if (player,action.card_index) in self.hints:
@@ -107,9 +128,9 @@ class MyAgent(agent.Agent):
                     self.hints[(player,action.card_index+i+1)] = set()
         # If we get a hint, update knowledge accordingly       
         elif action.type == HINT_COLOR:
-            self.color_hint = action.color
+            self.colorHintsToProcess[action.player] = action.color
         elif action.type == HINT_RANK:
-            self.rank_hint = action.rank
+            self.rankHintsToProcess[action.player] = action.rank
                    
 
 agent.register("mine", "Benjamin and Mark's Agent", MyAgent)
